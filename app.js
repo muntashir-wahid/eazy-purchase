d42eb525205775ec90152bd89e5b16e5ef159aa1
muntashir-wahid <muntashir.wahid@yahoo.com>
2023-10-07 10:00:30 +0600

    responding to undhanled routes

--- app.js
+++ app.js
@@ -14,4 +14,13 @@ if (process.env.NODE_ENV === "development") {
 app.use("/api/v1/products", productRouter);
 app.use("/api/v1/categories", categoryRouter);
 
+app.all("*", (req, res) => {
+  const message = `Can't find ${req.originalUrl} for ${req.method} on the sever`;
+
+  res.status(404).json({
+    status: "fail",
+    message,
+  });
+});
+
 module.exports = app;
